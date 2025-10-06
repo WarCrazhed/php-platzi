8851a7733d7c50db34ff37be2a8dcc47bd932745
Mario Zamora <zamora@Mac-mini-de-Mario-5.local>
2025-10-06 10:02:31 -0600

    Design of specialized prompts

--- src/OpenAiService.php
+++ src/OpenAiService.php
@@ -18,6 +18,13 @@ class OpenAiService
         $response = $this->client->chat()->create([
             'model' => 'gpt-3.5-turbo',
             'messages' => [
+                ['role' => 'system', 'content' => <<<EOT
+                    Eres un asistente especializado exclusivamente en PHP.
+                    - Si te preguntan algo que no este relacionado con PHP responde "Lo siento, solo puedo responder preguntas relacionadas con PHP."
+                    - Si te preguntan algo relacionado con PHPm responde de forma breve y concisa. Sin rodeos"
+                    - Responde en español.
+                    EOT
+                ],
                 ['role' => 'user', 'content' => 'Podrías responder en español la siguiente pregunta: '. $question],
             ],
         ]);
--- src/OpenRouterService.php
+++ src/OpenRouterService.php
@@ -32,7 +32,13 @@ class OpenRouterService
             'json' => [
                 'model' => 'deepseek/deepseek-chat-v3.1:free',
                 'messages' => [
-                    ['role' => 'system', 'content' => 'Eres un profesor de programación. Explica conceptos de manera clara y didáctica, siempre en español.'],
+                    ['role' => 'system', 'content' => <<<EOT
+                        Eres un asistente especializado exclusivamente en PHP.
+                        - Si te preguntan algo que no este relacionado con PHP responde "Lo siento, solo puedo responder preguntas relacionadas con PHP."
+                        - Si te preguntan algo relacionado con PHPm responde de forma breve y concisa. Sin rodeos"
+                        - Responde en español.
+                        EOT
+                    ],
                     ['role' => 'user', 'content' => $question],
 
                 ],
